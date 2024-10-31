3f7e33e9ad59efaf622d3dfe2c4996e834b9630a
Tom Dudley <dudleyt88@gmail.com>
2024-10-31 12:30:58 +0000

    Refactor 2015 day four

--- 2015/day_four.exs
+++ 2015/day_four.exs
@@ -2,34 +2,28 @@ defmodule DayFour do
   def partone(input) do
     input
     |> String.trim()
-    |> calc_hash(0, "")
+    |> calc_hash(0, "", 5)
   end
 
   def parttwo(input) do
     input
     |> String.trim()
-    |> calc_hash_parttwo(0, "")
+    |> calc_hash(0, "", 6)
   end
 
-  def calc_hash(key, current_num, current_hash) when binary_part(current_hash, 0, 5) == "00000" do
-    current_num
-  end
+  def calc_hash(key, current_num, current_hash, prefix_zeroes) do
+    desired_prefix = String.duplicate("0", prefix_zeroes)
 
-  def calc_hash(key, current_num, current_hash) do
-    next_num = current_num + 1
-    next_hash = :crypto.hash(:md5, key <> "#{next_num}") |> Base.encode16()
-    calc_hash(key, next_num, next_hash)
-  end
+    case String.length(current_hash) >= prefix_zeroes and
+           binary_part(current_hash, 0, prefix_zeroes) do
+      ^desired_prefix ->
+        current_num
 
-  def calc_hash_parttwo(key, current_num, current_hash)
-      when binary_part(current_hash, 0, 6) == "000000" do
-    current_num
-  end
-
-  def calc_hash_parttwo(key, current_num, current_hash) do
-    next_num = current_num + 1
-    next_hash = :crypto.hash(:md5, key <> "#{next_num}") |> Base.encode16()
-    calc_hash_parttwo(key, next_num, next_hash)
+      _ ->
+        next_num = current_num + 1
+        next_hash = :crypto.hash(:md5, key <> "#{next_num}") |> Base.encode16()
+        calc_hash(key, next_num, next_hash, prefix_zeroes)
+    end
   end
 end
 
