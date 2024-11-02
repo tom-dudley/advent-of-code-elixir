462bbb8d05a39ed9613721f718656143937d1298
Tom Dudley <dudleyt88@gmail.com>
2024-11-02 13:18:38 +0000

    2015 day six part two

--- 2015/day_six.exs
+++ 2015/day_six.exs
@@ -1,40 +1,50 @@
+# Note: This is very slow and inefficient, but does work 
 defmodule DaySix do
   def partone(input) do
-    grid = build_grid()
-
-    IO.puts("Built grid")
+    grid = build_grid(:off)
 
     commands =
       input
       |> String.split("\n", trim: true)
       |> Enum.map(fn x -> parse_command(x) end)
 
-    IO.puts("Parsed commands")
-
-    execute_all_commands(commands, grid)
+    execute_all_commands(commands, grid, &partone_algorithm/3)
     |> Map.values()
     |> Enum.count(fn x -> x == :on end)
   end
 
-  def build_grid() do
+  def parttwo(input) do
+    grid = build_grid(0)
+
+    commands =
+      input
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn x -> parse_command(x) end)
+
+    execute_all_commands(commands, grid, &parttwo_algorithm/3)
+    |> Map.values()
+    |> Enum.sum()
+  end
+
+  def build_grid(initial) do
     0..999
     |> Enum.flat_map(fn x ->
       0..999
       |> Enum.map(fn y -> {x, y} end)
     end)
-    |> Map.from_keys(:off)
+    |> Map.from_keys(initial)
   end
 
-  def execute_all_commands(commands, grid) when length(commands) == 0 do
+  def execute_all_commands(commands, grid, _algorithm) when length(commands) == 0 do
     grid
   end
 
-  def execute_all_commands([command | rest], grid) do
-    grid = execute_single_command(command, grid)
-    execute_all_commands(rest, grid)
+  def execute_all_commands([command | rest], grid, algorithm) do
+    grid = execute_single_command(command, grid, algorithm)
+    execute_all_commands(rest, grid, algorithm)
   end
 
-  def execute_single_command({command, [{x1, y1}, {x2, y2}]}, grid) do
+  def execute_single_command({command, [{x1, y1}, {x2, y2}]}, grid, algorithm) do
     IO.puts("Executing single command")
 
     coords =
@@ -44,30 +54,57 @@ defmodule DaySix do
         |> Enum.map(fn y -> {x, y} end)
       end)
 
-    execute_single_command_for_coord(command, coords, grid)
+    execute_single_command_for_coord(command, coords, grid, algorithm)
   end
 
-  def execute_single_command_for_coord(_command, remaining, grid) when length(remaining) == 0 do
+  def execute_single_command_for_coord(_command, remaining, grid, _algorithm)
+      when length(remaining) == 0 do
     grid
   end
 
-  def execute_single_command_for_coord(command, [coord | rest], grid) do
-    grid =
-      case command do
-        :on ->
-          Map.put(grid, coord, :on)
+  def execute_single_command_for_coord(command, [coord | rest], grid, algorithm) do
+    grid = algorithm.(command, coord, grid)
+    execute_single_command_for_coord(command, rest, grid, algorithm)
+  end
 
-        :off ->
-          Map.put(grid, coord, :off)
+  def partone_algorithm(command, coord, grid) do
+    case command do
+      :on ->
+        Map.put(grid, coord, :on)
 
-        :toggle ->
-          case Map.get(grid, coord) do
-            :on -> Map.put(grid, coord, :off)
-            :off -> Map.put(grid, coord, :on)
-          end
-      end
+      :off ->
+        Map.put(grid, coord, :off)
 
-    execute_single_command_for_coord(command, rest, grid)
+      :toggle ->
+        case Map.get(grid, coord) do
+          :on -> Map.put(grid, coord, :off)
+          :off -> Map.put(grid, coord, :on)
+        end
+    end
+  end
+
+  def parttwo_algorithm(command, coord, grid) do
+    case command do
+      :on ->
+        current_value = Map.get(grid, coord)
+        new_value = current_value + 1
+        Map.put(grid, coord, new_value)
+
+      :off ->
+        current_value = Map.get(grid, coord)
+        new_value = current_value - 1
+
+        if new_value < 0 do
+          Map.put(grid, coord, 0)
+        else
+          Map.put(grid, coord, new_value)
+        end
+
+      :toggle ->
+        current_value = Map.get(grid, coord)
+        new_value = current_value + 2
+        Map.put(grid, coord, new_value)
+    end
   end
 
   def parse_command("turn on " <> rest), do: parse_coords(:on, rest)
@@ -92,4 +129,4 @@ input =
   |> File.read!()
 
 IO.puts(DaySix.partone(input))
-# IO.puts(DaySix.parttwo(input))
+IO.puts(DaySix.parttwo(input))
