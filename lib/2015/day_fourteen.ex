81f20c512b56d8861ebf3b0e853ada4887ff397c
Tom Dudley <dudleyt88@gmail.com>
2024-11-13 20:30:47 +0000

    2015 day 14

--- lib/2015/day_fourteen.ex
+++ lib/2015/day_fourteen.ex
@@ -5,46 +5,120 @@ defmodule AdventOfCode2015.DayFourteen do
     load()
     |> String.split("\n", trim: true)
     |> Enum.map(&parse/1)
-    |> race(race_duration)
-  end
-
-  def race(reindeers, race_duration) do
-    reindeers
     |> Enum.map(fn reindeer ->
-      calculate_distance(reindeer, race_duration)
+      d = calculate_distance(reindeer, race_duration)
+
+      # assert that the length matches the number of seconds in the race
+      ^race_duration =
+        length(d)
+
+      d |> List.last()
     end)
     |> Enum.max()
   end
 
+  def parttwo() do
+    race_duration = 2503
+
+    reindeers =
+      load()
+      |> String.split("\n", trim: true)
+      |> Enum.map(&parse/1)
+
+    distances =
+      reindeers
+      |> Enum.map(fn reindeer ->
+        d = calculate_distance(reindeer, race_duration)
+        # assert that the length matches the number of seconds in the race
+        ^race_duration = length(d)
+        d
+      end)
+
+    distances
+    |> Enum.zip()
+    |> Enum.reduce(
+      List.duplicate(0, length(reindeers)),
+      fn x, scores ->
+        l = Tuple.to_list(x)
+        m = Enum.max(l)
+
+        0..(length(reindeers) - 1)
+        |> Enum.map(fn x ->
+          if Enum.at(l, x) == m do
+            Enum.at(scores, x) + 1
+          else
+            Enum.at(scores, x)
+          end
+        end)
+      end
+    )
+    |> Enum.max()
+  end
+
   def calculate_distance(reindeer, race_duration) do
-    step(reindeer, 0, 0, race_duration, :flying)
+    step(reindeer, [], 0, race_duration, :flying)
   end
 
-  def step(_reindeer, distance_covered, duration, race_duration, _state)
+  def step(_reindeer, distance_covered_by_second, duration, race_duration, _state)
       when duration >= race_duration do
-    distance_covered
+    distance_covered_by_second
   end
 
-  def step(reindeer, distance_covered, duration, race_duration, :flying) do
+  def step(reindeer, distance_covered_by_second, duration, race_duration, :flying) do
     fly_speed = reindeer |> elem(1) |> Map.get(:fly_speed)
     fly_duration = reindeer |> elem(1) |> Map.get(:fly_duration)
 
+    current_distance =
+      case List.last(distance_covered_by_second) do
+        nil ->
+          0
+
+        x ->
+          x
+      end
+
     if duration + fly_duration >= race_duration do
       time_left = race_duration - duration
-      distance_covered = distance_covered + fly_speed * time_left
+      fly_duration = time_left
+
+      new_distances =
+        1..fly_duration
+        |> Enum.map(fn x -> x * fly_speed + current_distance end)
+
+      distance_covered_by_second = Enum.concat(distance_covered_by_second, new_distances)
       duration = duration + fly_duration
-      step(reindeer, distance_covered, duration, race_duration, :resting)
+      step(reindeer, distance_covered_by_second, duration, race_duration, :resting)
     else
-      distance_covered = distance_covered + fly_speed * fly_duration
+      new_distances =
+        1..fly_duration
+        |> Enum.map(fn x -> x * fly_speed + current_distance end)
+
+      distance_covered_by_second = Enum.concat(distance_covered_by_second, new_distances)
       duration = duration + fly_duration
-      step(reindeer, distance_covered, duration, race_duration, :resting)
+      step(reindeer, distance_covered_by_second, duration, race_duration, :resting)
     end
   end
 
-  def step(reindeer, distance_covered, duration, race_duration, :resting) do
+  def step(reindeer, distance_covered_by_second, duration, race_duration, :resting) do
+    current_distance = List.last(distance_covered_by_second)
     rest_duration = reindeer |> elem(1) |> Map.get(:rest_duration)
-    duration = duration + rest_duration
-    step(reindeer, distance_covered, duration, race_duration, :flying)
+
+    if duration + rest_duration >= race_duration do
+      time_left = race_duration - duration
+      rest_duration = time_left
+
+      new_distances = List.duplicate(current_distance, rest_duration)
+
+      distance_covered_by_second = Enum.concat(distance_covered_by_second, new_distances)
+      duration = duration + rest_duration
+      step(reindeer, distance_covered_by_second, duration, race_duration, :flying)
+    else
+      new_distances = List.duplicate(current_distance, rest_duration)
+
+      distance_covered_by_second = Enum.concat(distance_covered_by_second, new_distances)
+      duration = duration + rest_duration
+      step(reindeer, distance_covered_by_second, duration, race_duration, :flying)
+    end
   end
 
   def parse(s) do
